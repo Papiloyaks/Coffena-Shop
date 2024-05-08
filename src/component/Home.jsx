c21ad6ca3b3886a4494ee89246a03fa8fe8df437
Yakub Yusuf <yakubyusuf608@gmail.com>
2024-05-08 08:50:31 +0100

    commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
               <button 
                 id="bn632-hover bn19"
                 type="button"
-                className="text-white w-40 bg-orange-400 hover:bg-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" >Book Table</button></Link>
+                className="text-white w-40 bg-orange-400 hover:bg-black focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center" >Book Table</button></Link>
               <button
                 data-collapse-toggle="navbar-cta"
                 type="button"
